0fe5a41910ffe0418a3286cc97b38642ca79a4a8
Bighnaraj Bhattamishra <bighna2005@gmail.com>
2025-07-19 19:50:51 +0530

    Upgraded the Blogcard components

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -5,14 +5,21 @@ import '../styles/BlogCard.css';
 function BlogCard({ post }) {
     return(
         <div className='blog-card'>
+        <div className="blog-content">
         <div className="blog-header">
-            <h3>{post.title}</h3>
-            <span className='="blog-category'>{post.category}</span>
+          <h3 className="blog-title">{post.title}</h3>
+          <span className="blog-category">{post.category}</span>
         </div>
-        <p>{post.excerpt}</p>
-        <div className="blog-footer">By {post.author}</div>
-        </div>       
-    );
+
+        <p className="blog-excerpt">{post.excerpt}</p>
+
+        <div className="blog-footer">
+          <span>By {post.author}</span>
+          <a href="#" className="read-more">Read More</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BlogCard;
