6338addcfcf001f50127717d2794e76abee33962
Bighnaraj Bhattamishra <bighna2005@gmail.com>
2025-07-20 20:25:26 +0530

    Added Login form layout with its css styling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-import '../styles/Login.css';
+import "../styles/Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Access your account to create & manage blogs</p>
+    <div className="login-wrapper">
+      <h1>Welcome Back</h1>
+      <p className="login-subtitle">Write & Manage Your Blogs</p>
+
+      <form className="login-form">
+        <input
+          type="email"
+          placeholder="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+
+        <p className="login-hint">
+          Don't have An Account Here? <a href="/register">Register</a>
+        </p>
+      </form>
     </div>
   );
 }
