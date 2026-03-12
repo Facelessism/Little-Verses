1cde67a96103d998a29decb12a3f05931468c05e
lost_fanatic <bighna2005@gmail.com>
2026-03-12 23:18:11 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -9,9 +9,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
-.catch(err => console.log(err));
+.catch(err => console.error(err));
