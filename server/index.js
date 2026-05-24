6a8527fa50024805007d1f8fd665350f25131727
lost_fanatic <bighna2005@gmail.com>
2026-05-24 14:55:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
